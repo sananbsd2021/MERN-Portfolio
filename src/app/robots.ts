99a2be10fa760cf859bf87ca0eb089ab796a0b83
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2026-02-07 21:51:09 +0700

    Update base URL for robots.txt configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'sanpf-cv.vercel.app';
 
     return {
         rules: {
